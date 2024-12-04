f023359a7034ed0d2a2fa17abbd8523fd18049a4
Jussi Jokinen <jussi.k.jokinen@gmail.com>
2024-12-04 23:51:32 +0200

    develop: fix - add and allow custom settings for the repository

---
+++ web/sites/default/settings.local.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Location of the site configuration files.
+ *
+ * The $settings['config_sync_directory'] specifies the location of file system
+ * directory used for syncing configuration data. On install, the directory is
+ * created. This is used for configuration imports.
+ *
+ * The default location for this directory is inside a randomly-named
+ * directory in the public files path. The setting below allows you to set
+ * its location.
+ */
+$settings['config_sync_directory'] = 'sites/default/files/config_WRzFCl_OkbXKLACVF9wbOoSFiq5IklGub4H7LBnqdF7dfgVepRxySvcu-gtjMa58nkMoxXkiMw/sync';
+
